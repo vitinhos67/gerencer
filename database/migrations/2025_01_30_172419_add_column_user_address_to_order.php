18966fbc9c5e36e4e4d33050c46c5f0fa678d434
Victor Santos da Silva <santtosvictor42@gmail.com>
2025-01-30 14:27:56 -0300

    chore: creating tables from orders

---
+++ database/migrations/2025_01_30_172419_add_column_user_address_to_order.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->foreignId('user_address_id')->nullable()->constrained('user_addresses')->onDelete('set null');
+        });
+    }
+};
