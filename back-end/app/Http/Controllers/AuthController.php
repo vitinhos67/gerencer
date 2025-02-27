dedaca4897926d56051f2d05c0d6f0a787cb9f35
Victor Santos da Silva <santtosvictor42@gmail.com>
2025-02-27 16:02:27 -0300

    return supplier to admin on login

--- back-end/app/Http/Controllers/AuthController.php
+++ back-end/app/Http/Controllers/AuthController.php
@@ -2,6 +2,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Suppliers\UserSupplier;
 use App\Models\User\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -11,19 +12,25 @@ class AuthController extends Controller
     public function login(Request $request)
     {
         $request->validate([
-            'email' => 'required|email',
+            'email'    => 'required|email',
             'password' => 'required',
         ]);
 
         $user = User::where('email', $request->email)->first();
 
-        if (!$user || !Hash::check($request->password, $user->password)) {
+        if (! $user || ! Hash::check($request->password, $user->password)) {
             return response()->json(['success' => false, 'errors' => 'invalid-credentials'], 401);
         }
 
         $token = $user->createToken('user_login')->plainTextToken;
 
-        return response()->json(['token' => $token], 200);
+        $response = ['token' => $token, "user" => $user];
+
+        if ($user->hasRole('admin')) {
+            $response['supplier'] = UserSupplier::where('user_id', $user->id)->first()->supplier;
+        }
+
+        return response()->json($response, 200);
     }
 
     public function logout(Request $request)
